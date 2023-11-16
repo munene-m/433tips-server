37784c0d32b0711b6d70f1d39a8a07249dd70bfb
munene-m <munenenjue18@gmail.com>
2023-11-16 14:53:06 +0300

    feat: added password reset feature where user receives email with link to reset their password

--- models/User.js
+++ models/User.js
@@ -1,49 +1,55 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    passResetToken: {
+      type: String,
+    },
+    // googleId: {
+    //   type: String,
+    //   // unique: true,
+    // },
+    // googleEmail: {
+    //   type: String,
+    //   // unique: true,
+    // },
+    // googleDisplayName: {
+    //   type: String,
+    // },
+    paid: {
+      type: Boolean,
+      default: false,
+    },
+    supreme: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  confirmPassword: {
-    type: String
-  },
-  country: {
-    type: String,
-    required: true
-  },
-  // googleId: {
-  //   type: String,
-  //   // unique: true,
-  // },
-  // googleEmail: {
-  //   type: String,
-  //   // unique: true,
-  // },
-  // googleDisplayName: {
-  //   type: String,
-  // },
-  paid: {
-    type: Boolean,
-    default: false
-  },
-  supreme:{
-    type:Boolean,
-    default: false
-  },
-  isAdmin: {
-    type:Boolean,
-    default: false
-  }
-},{timestamps: true});
+  { timestamps: true }
+);
 
 // userSchema.plugin(findOrCreate);
 
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,21 +1,33 @@
-const express = require('express')
-const { loginUser, registerUser, registerAdmin, getCredentials, getUser,getUsers,getVipUsers, deleteUser, reset, updateUser } = require("../controllers/authController");
-const { protect } = require("../middleware/authMiddleware")
+const express = require("express");
+const {
+  loginUser,
+  registerUser,
+  registerAdmin,
+  getCredentials,
+  getUser,
+  getUsers,
+  getVipUsers,
+  deleteUser,
+  changeUserPassword,
+  requestPasswordReset,
+  updateUser,
+} = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.route("/").get(protect,getUsers)
-router.route("/:id").get(protect, getUser)
-router.route("/credentials").get(protect,getCredentials)
-router.route("/getVip").get(protect, getVipUsers)
+router.route("/").get(protect, getUsers);
+router.route("/:id").get(protect, getUser);
+router.route("/credentials").get(protect, getCredentials);
+router.route("/getVip").get(protect, getVipUsers);
 // router.route("/googleCredentials").get(redirectUser)
-router.route("/register").post(registerUser)
-router.route("/register-admin").post(registerAdmin)
-router.route("/login").post(loginUser)
-router.route("/update/:id").put(updateUser)
-router.route("/reset").put(reset)
+router.route("/register").post(registerUser);
+router.route("/register-admin").post(registerAdmin);
+router.route("/login").post(loginUser);
+router.route("/update/:id").put(updateUser);
+router.route("/request-reset").post(requestPasswordReset);
+router.route("/change-password").post(changeUserPassword);
 // router.route("/auth/google").get(loginWithGoogle)
 // router.route("/auth/google/callback").get(googleAuthCallback);
-router.route("/delete/:id").delete(protect, deleteUser)
+router.route("/delete/:id").delete(protect, deleteUser);
 
-module.exports = router
-
+module.exports = router;
