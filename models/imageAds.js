7342fdc30d32572ee774d4d831ce760dd7fa6028
munene-m <munenenjue18@gmail.com>
2023-12-04 09:29:45 +0300

    chore: added ads route

---
+++ models/imageAds.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const adSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const imageAd = mongoose.model("ad", adSchema);
+module.exports = imageAd;
